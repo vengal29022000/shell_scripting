b98d498f78408dfac61957b7853910ab2045028f
VENGALAREDDY <vengalareddy.challa@gmail.com>
2025-01-04 02:06:01 +0530

    special characters

---
+++ if_condition.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+echo "enter the namber"
+read number
+if [ $number -gt 100]
+than
+    echo "the given number is greater than 100"
+else
+    echo "giben number is lessthan or equal to 100"
+fi
