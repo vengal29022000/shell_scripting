a7176ba82421f6e9b7f7b0a33ea5336d77f869cf
VENGALAREDDY <vengalareddy.challa@gmail.com>
2025-01-03 22:28:52 +0530

    Addition

--- Addition.sh
+++ Addition.sh
@@ -1,8 +1,8 @@
 #!bin/bash
-echo "please enter two numbers"
-num1=$1
+echo "please enter two numbers along sh commamd"
+num1=$1    # No space between = and value
 num2=$2
-sum=$(($num1+$num2))
+sum=$(($num1+$num2))   # Here ($num1+$num2) both number will add and the output is assigned to "sum" using "$" brfore ($num1+$num2)
 echo "the sum of ${num1} and ${num2} is : $sum"  
 
 
