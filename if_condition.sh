5cae4ee0fbd7259b4d53f223727289f8c86d763d
VENGALAREDDY <vengalareddy.challa@gmail.com>
2025-01-04 02:19:38 +0530

    special characters

--- if_condition.sh
+++ if_condition.sh
@@ -2,9 +2,9 @@
 
 echo "enter the namber"
 read number
-if [ $number -gt 100]
+if [ $number -gt 100 ]
 then
     echo "the given number is greater than 100"
 else
-    echo "giben number is lessthan or equal to 100"
+    echo "given number is lessthan or equal to 100"
 fi
